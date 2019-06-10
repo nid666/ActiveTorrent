bde4e2b64268181cfc906f0067684aa4bc2c4b4a
Samuel Leonte <samgleonte@gmail.com>
2019-06-10 16:49:12 -0400

    remove unneeded imports

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-#from selenium.webdriver.support import expected_conditions as EC
-#from selenium.webdriver.common.by import By
-#from selenium.webdriver.support.ui import WebDriverWait
 from bs4 import BeautifulSoup
 import requests
 from requests.auth import HTTPBasicAuth
