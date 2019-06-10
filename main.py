4a18d82cfdd439794fd2019be6278cfec60cb61c
Sid <35160315+nid666@users.noreply.github.com>
2019-06-10 04:14:34 -0400

    Update main.py
    It now lists out the raw links for each torrent on page 1

--- main.py
+++ main.py
@@ -1,38 +1,35 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-
-
-
+#from selenium.webdriver.support import expected_conditions as EC
+#from selenium.webdriver.common.by import By
+#from selenium.webdriver.support.ui import WebDriverWait 
 from bs4 import BeautifulSoup
 import requests
 from requests.auth import HTTPBasicAuth
+import time
 
-#SELEIUM STUFF FOR LOGIN! ~Sam
+#selenium used to pass login; requires firefox and geckodriver to work
 driver = webdriver.Firefox(executable_path='geckodriver.exe')
-def login():
-    driver.get("https://www.torrentleech.org/user/account/login/")
-    driver.find_element_by_name("username").send_keys("Samtheman801")
-    driver.find_element_by_name("password").send_keys("^^#XY9S1BWIoZ0ds")
-    driver.find_element_by_name("password").send_keys(Keys.ENTER)
+driver.get("https://www.torrentleech.org/torrents/browse/index/page/1")
+driver.find_element_by_name("username").send_keys("")
+driver.find_element_by_name("password").send_keys("")
+driver.find_element_by_name("password").send_keys(Keys.ENTER)
+time.sleep(2)
 
-login()
+soup = BeautifulSoup(driver.page_source, "lxml")
+#This part is just for testing purposes to see if the cookie bypass thing worked and makes the html look better
+#print(soup.prettify())
+
+number = 0
+#parse through index page and find all torrents; done by sorting for ONLY the css class="name"
+for link in soup.find_all("div", class_="name"):
+    number = (number + 1)
+    print(link)
+    
+
+    basename = 'link'
+    numberid = str(number)
+    fullna
 
 
-#we will have to set this part to the torrentleech html and the documentation has nothing about using cookies so we are going to have to figure out how to get our scraper past the login page
-#we can navigate the different torrentleech pages with https://www.torrentleech.org/torrents/browse/index/page/ followed by the page number
-
-website = requests.get("https://www.torrentleech.org/torrents/browse/index/page/1").text
-
-#This will be the cookies thing to hopefull bypass the login
-'''
-there was stuff here but I deleted it
-
-'''
-
-# this part here takes website which is that whole thing and sets it to the soup and uses the lxml parser which is a super fast one
-
-soup = BeautifulSoup(website, 'lxml')
-
-#This part is just for testing purposes but it gets the title of the html page and sets it to title then prints it out. Helpful in making sure we are scraping the right site
-title = soup.title
-print(soup)
+driver.close()
